e111dc12ec0f116fdd71347dda6a79033ebd7815
IsidoreSKO <sparagmos@hotmail.fr>
2024-09-18 17:46:42 +0200

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+// import Image from "next/image";
+"use client";
+
+export default function Home() {
+  return (
+    <main>
+      <button className="btn-primary btn">Click me!</button>
+    </main>
+  );
+}
